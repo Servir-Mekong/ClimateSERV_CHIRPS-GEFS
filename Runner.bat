4ffd2aabe509b0fff8ddf3b66caef57f0bb17059
mlaverdeb <mlaverdeb@gmail.com>
2020-09-15 15:36:02 +0700

    Updated runner

--- Runner.bat
+++ Runner.bat
@@ -1,5 +1,4 @@
 #call activate GPM-BICO
 python bin/ClimateServ_CHIPS-GEFS.py -i CHIRPS_GEFS_precip_mean -o CHIRPS_GEFSrange.zip -b 93,110,9,25 -s '2020-03-10' -t '2020-03-20' -n yes
-python bin/ClimateServ_CHIPS-GEFS.py -i CHIRPS_GEFS_precip_mean -o CHIRPS_GEFS10days.zip -b 93,110,9,25 
 pause
 
