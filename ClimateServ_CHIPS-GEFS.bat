60635bfcc50549e70305634d2ed3d8b39ef2e1b8
mlaverdeb <mlaverdeb@gmail.com>
2020-10-28 16:13:30 +0700

    include ascii format for postprocessing

--- ClimateServ_CHIPS-GEFS.bat
+++ ClimateServ_CHIPS-GEFS.bat
@@ -1,4 +1,4 @@
 #call activate GPM-BICO
-python bin/ClimateServ_CHIPS-GEFS.py -i CHIRPS_GEFS_precip_mean -o CHIRPS_GEFSrange.zip -b 70,110,9,35 -s '2020-03-10' -t '2020-03-20' -n yes 
+python bin/ClimateServ_CHIPS-GEFS.py -i CHIRPS_GEFS_precip_mean -o CHIRPS_GEFSrange.zip -b 70,110,9,35 -s '2020-03-10' -t '2020-03-20' -n ascii 
 pause
 
