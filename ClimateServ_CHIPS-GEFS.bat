e3e0a66e10c56b47deb9b81af9b5b9e0721d747d
mlaverdeb <mlaverdeb@gmail.com>
2020-11-03 11:18:17 +0700

    Update ClimateServ_CHIPS-GEFS.bat

--- ClimateServ_CHIPS-GEFS.bat
+++ ClimateServ_CHIPS-GEFS.bat
@@ -1,4 +1,4 @@
 call activate GPM-BICO
-python bin/ClimateServ_CHIPS-GEFS.py -i CHIRPS_GEFS_precip_mean -o CHIRPS_GEFSrange.zip -b 70,110,9,35 -s '2020-03-10' -t '2020-03-20' -n ascii 
+python bin/ClimateServ_CHIPS-GEFS.py -i CHIRPS_GEFS_precip_mean -o CHIRPS_GEFSrange.zip -b 89,112,3,36 -s '2020-09-21' -t '2020-10-06' -n ascii
 pause
 
